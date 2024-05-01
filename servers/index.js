f7868e2e6d512951b3e1478cfe4c02c1b4cf7dd8
ujjwal9019 <ujjwal9019@gmail.com>
2024-05-01 11:18:24 +0530

    Url ,  Url Parsing  , query handling etc

--- servers/index.js
+++ servers/index.js
@@ -1,14 +1,23 @@
  const http = require("http");
 const fs = require("fs");
+const url = require("url");
 //  to create server
 // from this you can get many information about serever like kha se request aa rhi hai uska url ip address etc or hum usse log file me save krwa sakte hai
  const myserver = http.createServer((req , res) => {
+    if(req.url === "/favicon.ico") return res.end();
     const log = `${Date.now()} ${req.url}: New Request Reciived at time\n`;
+    // Now we can extract query parameter by writing true
+  const Myurl = url.parse(req.url ,true);
+
+    console.log(Myurl);
     fs.appendFile("log.txt" , log , (err , data) => {
-        switch(req.url){
+//    We can now redirect to exact pathname irresepective of query parameteres
+        switch(Myurl.pathname){
             case "/" : res.end("hompage");
             break;
-            case "/about" : res.end("about")
+            case "/about" : 
+            const username = Myurl.query.myname;
+            res.end(`hii , ${username}`);
             break;
             default:
                 res.end("404 Not Found"); 
